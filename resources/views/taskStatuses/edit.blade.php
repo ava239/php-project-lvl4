16bbeecab78843366895fb107f660c8d6cb02e7c
ava <ava239@yandex.ru>
2020-10-03 20:36:22 +0300

    forms attributes

--- resources/views/taskStatuses/edit.blade.php
+++ resources/views/taskStatuses/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <h1 class="mb-5">{{ __('task_statuses.edit_title') }}</h1>
-        {!! Form::open()->fill($taskStatus)->route('task_statuses.update', [$taskStatus])->method('PATCH') !!}
+        {!! Form::open()->fill($taskStatus)->route('task_statuses.update', [$taskStatus])->method('PATCH')->attrs(['class' => 'w-50']) !!}
         @include('taskStatuses.form')
         <div>
             {!! Form::submit(__('task_statuses.update')) !!}
