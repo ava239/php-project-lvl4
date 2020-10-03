0ab07da61cbaeb0137c44cd455c558a0b0678200
ava <ava239@yandex.ru>
2020-10-03 20:20:57 +0300

    changed auth rule in task statuses view

--- resources/views/taskStatuses/index.blade.php
+++ resources/views/taskStatuses/index.blade.php
@@ -2,18 +2,18 @@
 @section('content')
     <div class="container">
         <h1 class="mb-5">{{ __('task_statuses.title') }}</h1>
-        @can('create', App\TaskStatus::class)
+        @auth
             <a href="{{ route('task_statuses.create') }}" class="btn btn-primary">{{ __('task_statuses.add_new') }}</a>
-        @endcan
+        @endauth
         <table class="table mt-2">
             <thead>
             <tr>
                 <th>{{ __('task_statuses.id') }}</th>
                 <th>{{ __('task_statuses.name') }}</th>
                 <th>{{ __('task_statuses.created_at') }}</th>
-                @canany(['delete','update'], App\TaskStatus::class)
-                <th>{{ __('task_statuses.actions') }}</th>
-                @endcanany
+                @auth
+                    <th>{{ __('task_statuses.actions') }}</th>
+                @endauth
             </tr>
             </thead>
             @foreach($taskStatuses as $taskStatus)
@@ -21,21 +21,22 @@
                     <td>{{ $taskStatus->id }}</td>
                     <td>{{ $taskStatus->name }}</td>
                     <td>{{ $taskStatus->created_at }}</td>
-                    @canany(['delete','update'], $taskStatus)
-                    <td>
-                        @can('delete', $taskStatus)
-                            <a href="{{ route('task_statuses.destroy', $taskStatus) }}" data-confirm="{{ __('task_statuses.confirmation') }}"
-                               data-method="delete">
-                                {{ __('task_statuses.remove') }}
-                            </a>
-                        @endcan
-                        @can('update', $taskStatus)
-                            <a href="{{ route('task_statuses.edit', $taskStatus) }}">
-                                {{ __('task_statuses.edit') }}
-                            </a>
-                        @endcan
-                    </td>
-                    @endcanany
+                    @auth
+                        <td>
+                            @can('delete', $taskStatus)
+                                <a href="{{ route('task_statuses.destroy', $taskStatus) }}"
+                                   data-confirm="{{ __('task_statuses.confirmation') }}"
+                                   data-method="delete">
+                                    {{ __('task_statuses.remove') }}
+                                </a>
+                            @endcan
+                            @can('update', $taskStatus)
+                                <a href="{{ route('task_statuses.edit', $taskStatus) }}">
+                                    {{ __('task_statuses.edit') }}
+                                </a>
+                            @endcan
+                        </td>
+                    @endauth
                 </tr>
             @endforeach
         </table>
