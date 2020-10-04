7396a131f661cfaa0b8231736cf2821a46f55a19
ava <ava239@yandex.ru>
2020-10-04 21:18:03 +0300

    fix html error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,9 +34,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <a class="nav-link @if(Route::is('tasks.index')) active @endif" href="{{ route('tasks.index') }}">{{ __('tasks.menu_link') }}</a>
-                        <a class="nav-link @if(Route::is('task_statuses.index')) active @endif" href="{{ route('task_statuses.index') }}">{{ __('task_statuses.menu_link') }}</a>
-                        <a class="nav-link @if(Route::is('labels.index')) active @endif" href="{{ route('labels.index') }}">{{ __('labels.menu_link') }}</a>
+                        <li><a class="nav-link @if(Route::is('tasks.index')) active @endif" href="{{ route('tasks.index') }}">{{ __('tasks.menu_link') }}</a></li>
+                        <li><a class="nav-link @if(Route::is('task_statuses.index')) active @endif" href="{{ route('task_statuses.index') }}">{{ __('task_statuses.menu_link') }}</a></li>
+                        <li><a class="nav-link @if(Route::is('labels.index')) active @endif" href="{{ route('labels.index') }}">{{ __('labels.menu_link') }}</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
