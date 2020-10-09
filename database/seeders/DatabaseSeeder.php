65fd54efa64d19316d1c592712e7bf164cd38759
ava <ava239@yandex.ru>
2020-10-10 00:31:40 +0300

    now seeder creates at least few of each entities

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Label;
+use App\Models\Task;
 use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
@@ -23,5 +26,9 @@ class DatabaseSeeder extends Seeder
                 ['name' => 'finished'],
             )
         )->create();
+
+        Label::factory()->count(3)->create();
+        User::factory()->count(2)->create();
+        Task::factory()->count(3)->create();
     }
 }
